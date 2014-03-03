787a87c6620f84fe1ed048e7213aa5b60325b783
Brant Unger <brantunger@gmail.com>
2014-03-03 12:36:01 -0700

    Buildable portion of yelp UI only

--- app/src/main/java/groupo/travellight/app/LoginActivity.java
+++ app/src/main/java/groupo/travellight/app/LoginActivity.java
@@ -265,8 +265,8 @@ public class LoginActivity extends Activity {
             showProgress(false);
 
             if (success) {
-                Intent intent = new Intent (getApplicationContext(), TripsActivity.class);
-                startActivity(intent);
+               // Intent intent = new Intent (getApplicationContext(), TripsActivity.class);
+               // startActivity(intent);
                 //finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
